2f46b54d0c08b39f725704c474324788c973db39
Gregor Ottmann <zaphod@bluephod.net>
2018-08-05 08:50:42 +0200

    Implemented a way to cleanly stop the service by creating a killfile.

--- src/main/java/net/bluephod/henkinson/Henkinson.java
+++ src/main/java/net/bluephod/henkinson/Henkinson.java
@@ -1,120 +1,48 @@
 package net.bluephod.henkinson;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import com.diozero.ws281xj.LedDriverInterface;
 import com.diozero.ws281xj.PixelAnimations;
 import com.diozero.ws281xj.PixelColour;
 import com.diozero.ws281xj.rpiws281x.WS281x;
+import org.pmw.tinylog.Logger;
 
 public class Henkinson {
-	public static void main(String[] args) {
-		int gpio_num = 18;
-		//int gpio_num = 10;
-		int brightness = 255;  // 0..255
-		//int num_pixels = 12;
-		int num_pixels = 60;
+	private static final Path KILL_FILE = Paths.get("/home/pi/henkinson/killfile");
 
-		System.out.println("Using GPIO " + gpio_num);
+	public static void main(String[] args) throws Exception {
+		// must be 18 or 10 - those pins can do PWM
+		int gpioNum = 18;
 
-		try(LedDriverInterface led_driver = new WS281x(gpio_num, brightness, num_pixels)) {
-			rainbowColours(led_driver);
-			test2(led_driver);
-			hsbTest(led_driver);
-			hslTest(led_driver);
+		// 0..255
+		int brightness = 64;
 
+		// number of LEDs in the strip
+		int numPixels = 60;
+
+		Logger.info(String.format("Using GPIO %d", gpioNum));
+
+		try(LedDriverInterface ledDriver = new WS281x(gpioNum, brightness, numPixels)) {
 			while(true) {
-				System.out.println("loop()");
-				PixelAnimations.demo(led_driver);
-			}
-		}
-	}
+				PixelAnimations.colourWipe(ledDriver, PixelColour.createColourRGB(255, 0, 0), 25); // Red
+				PixelAnimations.colourWipe(ledDriver, PixelColour.createColourRGB(0, 0, 255), 25); // Blue
 
-	private static void rainbowColours(LedDriverInterface ledDriver) {
-		System.out.println("rainbowColours()");
-
-		int[] colours = PixelColour.RAINBOW;
-
-		for(int i = 0; i < 250; i++) {
-			for(int pixel = 0; pixel < ledDriver.getNumPixels(); pixel++) {
-				ledDriver.setPixelColour(pixel, colours[(i + pixel) % colours.length]);
-			}
-
-			ledDriver.render();
-			PixelAnimations.delay(50);
-		}
-	}
-
-	private static void test2(LedDriverInterface ledDriver) {
-		System.out.println("test2()");
-
-		// Set all off
-		ledDriver.allOff();
-
-		int delay = 20;
-
-		// Gradually add red
-		System.out.println("Adding red...");
-		for(int i = 0; i < 256; i += 2) {
-			for(int pixel = 0; pixel < ledDriver.getNumPixels(); pixel++) {
-				ledDriver.setRedComponent(pixel, i);
-			}
-
-			ledDriver.render();
-			PixelAnimations.delay(delay);
-		}
-
-		// Gradually add green
-		System.out.println("Adding green...");
-		for(int i = 0; i < 256; i += 2) {
-			for(int pixel = 0; pixel < ledDriver.getNumPixels(); pixel++) {
-				ledDriver.setGreenComponent(pixel, i);
-			}
-
-			ledDriver.render();
-			PixelAnimations.delay(delay);
-		}
-
-		// Gradually add blue
-		System.out.println("Adding blue...");
-		for(int i = 0; i < 256; i += 2) {
-			for(int pixel = 0; pixel < ledDriver.getNumPixels(); pixel++) {
-				ledDriver.setBlueComponent(pixel, i);
-			}
-
-			ledDriver.render();
-			PixelAnimations.delay(delay);
-		}
-
-		// Set all off
-		ledDriver.allOff();
-	}
-
-	private static void hsbTest(LedDriverInterface ledDriver) {
-		System.out.println("hsbTest()");
-		float brightness = 0.5f;
-
-		for(float hue = 0; hue < 1; hue += 0.05f) {
-			for(float saturation = 0; saturation <= 1; saturation += 0.05f) {
-				for(int pixel = 0; pixel < ledDriver.getNumPixels(); pixel++) {
-					ledDriver.setPixelColourHSB(pixel, hue, saturation, brightness);
+				if(serviceShouldStop()) {
+					Logger.info(String.format("Kill file (%s) found, deleting and exiting.", KILL_FILE));
+					break;
 				}
-				ledDriver.render();
-				PixelAnimations.delay(20);
 			}
 		}
+
+		Logger.info("Exiting Henkinson.");
+		System.exit(0);
 	}
 
-	private static void hslTest(LedDriverInterface ledDriver) {
-		System.out.println("hslTest()");
-		float luminance = 0.5f;
-
-		for(float hue = 0; hue < 360; hue += (360 / 20)) {
-			for(float saturation = 0; saturation <= 1; saturation += 0.05f) {
-				for(int pixel = 0; pixel < ledDriver.getNumPixels(); pixel++) {
-					ledDriver.setPixelColourHSL(pixel, hue, saturation, luminance);
-				}
-				ledDriver.render();
-				PixelAnimations.delay(20);
-			}
-		}
+	private static boolean serviceShouldStop() throws IOException {
+		return Files.deleteIfExists(KILL_FILE);
 	}
 }
